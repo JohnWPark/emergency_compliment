2726df71f77e4d428432dee84152b2b19a9ad9d3
Bryce Lively <bryce.lively@gmail.com>
2015-12-01 10:52:26 -0500

    brackets in complimentsController

--- controllers/complimentsController.js
+++ controllers/complimentsController.js
@@ -23,12 +23,12 @@ var complimentsController = {
       res.render('compliments/edit.hbs', {
           compliment: Compliment.find(req.params.id)
       })
-  }
+  },
 //HEY IM MAKING A CHANGE THAT SHOULD FORCE A MERGE CONFLICT!
  // update action code goes here...
-},
 update: function(req, res) {
   // update action
+  }
 }
 
 module.exports = complimentsController;
